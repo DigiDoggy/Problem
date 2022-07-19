535295ad43122bea0b666b1b4eb0d4209d45ed2d
your name <you@example.com>
2022-07-19 23:43:40 +0300

    Create example

---
+++ src/Problems/ProblemExsample.java
@@ -0,0 +1,29 @@
+package Problems;
+
+public class ProblemExsample {
+    public static void main(String[] args) {
+
+        /*
+                            That's an example.
+                You can post code you don't understand.
+        Or if you have a problem and the code doesn't work for some reason.
+
+
+
+        */
+
+
+
+        //Exercise.
+        //Write a multiplication table using a loop.
+
+            for (int i = 1; i < 10; i++) {
+                for (int j = 1; j < 10; j++) {
+                    System.out.println("%d\t", i * j); // Why doesn't it work. Example
+                }
+                System.out.println();
+            }
+        }
+
+    }
+
